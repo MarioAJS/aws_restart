05235e1d442bc55702d715397a9aba637c13a340
MarioAJS <mario.a.jimenez.s@gmail.com>
2023-10-12 20:57:31 -0400

    Added scripts
    Added scripts from latest LABs

---
+++ analyze-insulin.py
@@ -0,0 +1,4 @@
+"""
+LAB 118 project> Analyze insulin
+Mario Jimenez Saucedo
+"""
---
+++ jsonFileHandler.py
@@ -0,0 +1,17 @@
+"""
+LAB 116 json handler
+Mario Jimenez Saucedo
+"""
+
+import json
+
+def readJsonFile(fileName):
+    data=""
+    try:
+        with open('files/insulin.json') as json_file:
+            data = json.load(json_file)
+    except IOError:
+        print("Could not read file")
+    return data
+
+    
